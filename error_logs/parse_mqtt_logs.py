fcf680ecc7bc139f43f4cc9395ed741353af0f09
yapping72@gmail.com <yapping72@gmail.com>
2023-06-19 18:43:37 +0800

    Update mqtt log formatter

--- error_logs/parse_mqtt_logs.py
+++ error_logs/parse_mqtt_logs.py
@@ -1,15 +1,19 @@
 import datetime
 import os
 
-log_messsage = []
+log_messages = []
 
-file_path = os.path.join(".","mqtt_logs.txt")
+file_path = os.path.join(".", "mqtt_logs.txt")
 with open(file_path, 'r') as file:
-	lines = file.readlines()
+    lines = file.readlines()
 
 for line in lines:
-	timestamp, message = line.split(": ", 1)
-	dt = datetime.datetime.fromtimestamp(int(timestamp))
-	formatted_date = dt.strftime('%d-%m-%Y %H:%M:%S')
-	print(f"{formatted_date}: {message}")
-    
+    timestamp, message = line.split(": ", 1)
+    timestamp = timestamp.strip()
+    dt = datetime.datetime.fromtimestamp(int(timestamp))
+    formatted_date = dt.strftime('%d-%m-%Y %H:%M:%S')
+    log = f"{formatted_date}: {message}"
+    log_messages.append(log)
+
+with open(file_path, 'w') as file:
+    file.writelines(log_messages)
