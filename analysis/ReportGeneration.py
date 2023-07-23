3f6f172ae712c0177946ea7fcedd9dd1ec53de4c
Danial Hazeem <danialhbma@gmail.com>
2023-07-23 18:07:52 +0800

    added some validation checks for report generation

--- analysis/ReportGeneration.py
+++ analysis/ReportGeneration.py
@@ -5,7 +5,6 @@ from reportlab.pdfgen import canvas
 from PIL import Image
 
 def convert_images_to_pdf(image_directory, output_pdf_name=None):
-    # Get the absolute path of the script's directory
     script_directory = os.path.dirname(os.path.abspath(__file__))
 
     # Combine the script's directory with the "reports" folder to get the absolute path of the reports directory
@@ -27,10 +26,7 @@ def convert_images_to_pdf(image_directory, output_pdf_name=None):
     # Get a list of image files from the specified directory - will take all files in the directory
     image_files = [f for f in os.listdir(image_directory) if os.path.isfile(os.path.join(image_directory, f))]
 
-    # Sort the image files to maintain order
     image_files.sort()
-
-    # Set the position and dimensions for the images in the PDF
     page_width, page_height = A4
 
     # Loop through the image files and add them to the PDF
@@ -39,20 +35,16 @@ def convert_images_to_pdf(image_directory, output_pdf_name=None):
         img = Image.open(image_path)
         img_width, img_height = img.size
 
-        # Calculate the scaling factor to fit the entire image within one page
         width_ratio = page_width / img_width
         height_ratio = page_height / img_height
         scaling_factor = min(width_ratio, height_ratio)
 
-        # Calculate the scaled dimensions of the image
         scaled_width = img_width * scaling_factor
         scaled_height = img_height * scaling_factor
 
-        # Calculate the position to center the image on the page
         x_offset = (page_width - scaled_width) / 2
         y_offset = (page_height - scaled_height) / 2
 
-        # Draw the scaled image on the canvas
         c.drawInlineImage(img, x_offset, y_offset, width=scaled_width, height=scaled_height)
 
         # Add a new page for the next image
@@ -63,23 +55,18 @@ def convert_images_to_pdf(image_directory, output_pdf_name=None):
 
     # Save the PDF file with a dynamic name including the current month
     current_month = datetime.now().strftime("%B")
-    # Get the absolute path of the script's directory
     script_directory = os.path.dirname(os.path.abspath(__file__))
     pdf_output_path = os.path.join(script_directory, "reports", f"Analysis_{current_month}_{current_year}.pdf")
     c.save()
 
-    # Print the location where the PDF file was saved
     print(f"PDF file saved at: {pdf_output_path}")
 
 if __name__ == "__main__":
-    # Use the relative path to the image directory from the current script location
     image_directory = "results"
 
-    # Get the absolute path of the script's directory
     script_directory = os.path.dirname(os.path.abspath(__file__))
 
     # Combine the script's directory with the image_directory to get the absolute path of the image directory
     image_directory_absolute = os.path.join(script_directory, image_directory)
 
-    # Convert images to PDF and save with dynamic file name
     convert_images_to_pdf(image_directory_absolute)
