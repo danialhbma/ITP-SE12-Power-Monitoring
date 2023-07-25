f45bbc955b515eae668cf4f4d657e690bc95b887
ong-yi-xuan <2102623@sit.singaporetech.edu.sg>
2023-07-25 10:34:26 +0800

    add estimation code in api, to fix import issue

--- docker-containers/api/app/calculator_app.py
+++ docker-containers/api/app/calculator_app.py
@@ -1,17 +1,48 @@
 from flask import Flask, request, jsonify
+import pandas as pd
+import os
+from collections import defaultdict
+from analysis.calculate_power_consumption import *
 
 app = Flask(__name__)
 
 @app.route("/calculate", methods=["POST"])
 def calculate():
-    # To replace with actual input and formula
-    aircon_power_consumption = request.json["aircon_input"]
-    light_power_consumption = request.json["light_input"]
-    water_power_consumption =  request.json["water_input"]
-    result = aircon_power_consumption + light_power_consumption + water_power_consumption
-
+    # get user inputs
+    num_of_purple_led_racks = request.json["num_of_purple_led_racks"]
+    num_of_white_led_racks = request.json["num_of_white_led_racks"]
+    aircon_cooling_capacity = request.json["aircon_cooling_capacity"]
+    aircon_number_of_hours_ran = request.json["aircon_number_of_hours_ran"]
+    
+    pc_data_dict, number_of_days_observed = get_pc_data()
+    if pc_data_dict is not None:
+        # get estimated power consumption based on user inputs
+        water_power_consumption = get_water_power_consumption(pc_data_dict,
+                                                            num_of_purple_led_racks + num_of_white_led_racks)
+        purple_led_power_consumption, white_led_power_consumption = get_racklight_power_consumption(pc_data_dict,
+                                                                                                    num_of_purple_led_racks,
+                                                                                                    num_of_white_led_racks)
+        if aircon_cooling_capacity == 'Average':
+            aircon_power_consumption = get_avg_aircon_power(number_of_days_observed, aircon_number_of_hours_ran)
+        else:
+            aircon_power_consumption = get_max_aircon_power(number_of_days_observed, aircon_number_of_hours_ran)
+        
+        result = water_power_consumption + purple_led_power_consumption + white_led_power_consumption + aircon_power_consumption
+        
+        # get current power consumption based on default values
+        current_power_consumption = get_water_power_consumption(pc_data_dict) + get_racklight_power_consumption(pc_data_dict) + aircon_power_consumption
+        
+        # get difference in power consumption
+        difference = result - current_power_consumption
+        
+    else:
+        result = None
+        difference = None
+        
     # Return result to client
-    return jsonify({"result": result})
+    return jsonify({
+        "result": result,
+        "diff": difference})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080) # enable on port 8080 for all available ip addresses
